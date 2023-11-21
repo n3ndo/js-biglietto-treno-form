83d88b0fdf9c9006984fdc5f1e22e456352de099
n3ndo <ferdinandof3lixbagnale@gmail.com>
2023-11-21 21:54:41 +0100

    [UPDATE] added style at data section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 let km;
 let eta;
+let name;
 let biglietto_treno;
 const button = document.querySelector('.btn');
 
 button.addEventListener("click", function(){
     let km = document.getElementById("km").value;
     let eta = document.getElementById("eta").value;
-
+    let name = document.getElementById("name").value;
     console.log(km);
     console.log(eta);
     if (eta < 18) {
@@ -17,6 +18,6 @@ button.addEventListener("click", function(){
         biglietto_treno = km * 0.21;
     }
     biglietto_treno = biglietto_treno.toFixed(2);
-    console.log(biglietto_treno);
+    document.getElementById("test").innerHTML = `NOME PASSEGGERO: ${name} <br> PREZZO: ${biglietto_treno}€`;
 })
 
